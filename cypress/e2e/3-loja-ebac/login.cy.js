51477384df844b02ec517c940a7edfe94fdcfa5e
TamaraSQA <tamara.n.batista@hotmail.com>
2024-10-31 16:44:52 +1100

    primeiro teste automatizado

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,15 +1,16 @@
-/// <reference type="cypress"/>
+///reference types="cypress"/>
 
 describe('Funcionalidade: Login', () => {
 
-    it('Deve fazer login com sucesso', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
-        cy.get('#username').type('tamara.teste@teste.com.br')
-        cy.get('#password').type('Abc@1234')
-        cy.get('.woocommerce-form > .button').click()
-        
-        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, tamara.teste (não é tamara.teste? Sair)')
-    
+it('Deve fazer login com sucesso', () => {
+    cy.visit('http://lojaebac.ebaconline.art.br/my-account/')
+    cy.get('#username').type('tamara.teste2@teste.com.br')
+    cy.get('#password').type('teste@123')
+    cy.get('.woocommerce-form > .button').click()
+
+    cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, tamara.teste2 (não é tamara.teste2? Sair)')
+
+
     })
 
 })
